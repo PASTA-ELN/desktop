5220f5da2cba705eaaec9dfa7a8434f6fb562296
Dr. Steffen Brinckmann <sbrinckm@gmail.com>
2022-06-19 21:59:14 +0200

    update version numbers

--- checkAllVersions.py
+++ checkAllVersions.py
@@ -452,7 +452,7 @@ def gitNewVersion(msg, version=None):
       #create new version of GUI
       os.system('npm run build')
       shutil.copyfile('dist/pasta-'+version[1:]+'.AppImage'  ,'../Desktop/pasta-linux.AppImage')
-      shutil.copyfile('dist/PASTA-'+version[1:]+'-mac.tar.gz','../Desktop/PASTA-mac.tar.gz')
+      # shutil.copyfile('dist/PASTA-'+version[1:]+'-mac.tar.gz','../Desktop/PASTA-mac.tar.gz')
       shutil.copyfile('dist/PASTA Setup '+version[1:]+'.exe' ,'../Desktop/PASTA-Setup-win.exe')
     os.system('git commit -a -m "update version numbers"')
     os.system('git tag -a '+version+' -m "'+msg+'"')
